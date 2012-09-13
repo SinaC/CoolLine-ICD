f4adfdf3d41b4aec3a051b92290e8d38303dd96c
Joel Heymbeeck <heymbeeck.joel@gmail.com>
2012-09-13 22:05:57 +0200

    Version 1.0 release
    strip-lib added
    unused code removed

--- CoolLine-ICD/core.lua
+++ CoolLine-ICD/core.lua
@@ -1,17 +1,14 @@
 local CoolLine = CoolLine
-assert(CoolLine, "CoolLine not found") -- TODO: locales
+assert(CoolLine, "CoolLine not found")
 
 local LibICD = LibStub("LibInternalCooldowns-1.0")
-assert(LibICD, "LibInternalCooldowns-1.0 not found") -- TODO: locales
-
---print("CoolLineICD")
+assert(LibICD, "LibInternalCooldowns-1.0 not found")
 
 local CoolLineICD = CreateFrame("Frame")
 
 function CoolLineICD:InternalCooldowns_TalentProc(callback, spellID, start, duration)
 	local name, _, icon = GetSpellInfo(spellID)
 	local endtime = start + duration
---print("InternalCooldowns_TalentProc:"..tostring(spellID).."  "..tostring(start).."  "..tostring(duration).."  "..tostring(name))
 	CoolLine.NewCooldown(name, icon, endtime, true)
 end
 
@@ -19,7 +16,6 @@ function CoolLineICD:InternalCooldowns_Proc(callback, itemID, spellID, start, du
 	local icon = select(10, GetItemInfo(itemID))
 	local name = GetItemInfo(itemID)
 	local endtime = start + duration
---print("InternalCooldowns_Proc:"..tostring(itemID).."  "..tostring(spellID).."  "..tostring(start).."  "..tostring(duration).."  "..tostring(name))
 	CoolLine.NewCooldown(name, icon, endtime, true)
 end
 
